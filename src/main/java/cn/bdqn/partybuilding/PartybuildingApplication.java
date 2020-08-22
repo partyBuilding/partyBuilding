365f49c268d698ba5c60ded47e887d56ac8c5b46
780174514 <780174514@qq.com>
2020-08-22 14:41:48 +0800

    初始化项目

---
+++ src/main/java/cn/bdqn/partybuilding/PartybuildingApplication.java
@@ -0,0 +1,13 @@
+package cn.bdqn.partybuilding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartybuildingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartybuildingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/bdqn/partybuilding/PartybuildingApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bdqn.partybuilding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartybuildingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
